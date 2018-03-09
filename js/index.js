05a2726140bff2d649306751518de28c46787f7d
Kachniss <vopalkova.katerina@gmail.com>
2018-03-09 13:55:15 -0800

    Top Story API, limit articles, article images

--- js/index.js
+++ js/index.js
@@ -1,50 +1,38 @@
 // main js file
 $(function(){ 
-    var articlesLoad = 12; // number of articles wanted with an image
+    var articlesLoad = 12; // number of articles wanted
     $('.category').change(function(){
         articlesLoad = 12;
         $(".articles").addClass("loader");
         $("header").addClass("header-small");
         $("footer").addClass("footer-small");
         $(".articles").empty();
-        var value = $(".category").val();
-        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-            url += '?' + $.param({
-            'api-key': "1f73ed52c30d4864bb4d5b310aec39fc"
-            });
-            url +=  "&fq=news_desk:('" + value + "')";
-            // url +=  "&fq=news_desk:('Sports' 'Foreign')";
-            // url += "&fq=page:(0%201)";
-            
+        var section = $(".category").val();
+        var apiKey = "1f73ed52c30d4864bb4d5b310aec39fc";
+        var url = "http://api.nytimes.com/svc/topstories/v2/" + section +".json?api-key=" + apiKey;
         callAjax(url);
-        // url += "&page=1";
-        // callAjax(url);
     });
 
+    // Ajax call function
     function callAjax(url) {  
         $.ajax({
             method: "GET",
             url: url,
-            // async: false,
           }).done(function(data) {
-            //   console.log(data);
-              var articles = data.response.docs;
-              var image = "";
+              var articles = data.results;
               var imageNumber = 0;
-              for (var i = 0; i < 10; i++) {
-                // testing subtype of multimedia to find the best quality picture (if there is any)
-                for (var j = 0; j < articles[i].multimedia.length; j++) {
-                    if (articles[i].multimedia[j].subtype === "xlarge") {
-                        image = articles[i].multimedia[j].subtype;
-                        imageNumber = j;
+              for (var i = 0; i < articles.length; i++) {
+                // finding the best quality picture (if there is any)
+                if(articles[i].multimedia.length==5){
+                    var image = articles[i].multimedia[4].url;
+                    if (image&&(articlesLoad>0)) {
+                        $(".articles").append("<a href='" + articles[i].url + "' class='article' style='background:url(" + image + ") no-repeat center/cover;'><li><p class='article-text'>" + articles[i].abstract + "</p></li></a>");
                         articlesLoad--;
-                        break;
                     }
-                }
-                // appending the article to the list if there is an image and no. of images left to load is >= 0
-                if (image&&(articlesLoad>=0)) {
-                    $(".articles").append("<a href='" + articles[i].web_url + "' class='article' style='background:url(https://www.nytimes.com/" + articles[i].multimedia[imageNumber].url + ") no-repeat center/cover;'><li><p class='article-text'>" + articles[i].snippet + "</p></li></a>");
-                }  
+                    if (articlesLoad==0) {
+                        break;
+                    }   
+                }       
               }
           }).fail(function(error) {
             throw error;
