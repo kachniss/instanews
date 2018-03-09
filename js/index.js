b305c757d32b4afb9af28168cee724598f3d0406
Kachniss <vopalkova.katerina@gmail.com>
2018-03-09 13:18:07 -0800

    Add loader, header styles

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 // main js file
 $(function(){ 
-    // $(".loader").hide();
+    var articlesLoad = 12; // number of articles wanted with an image
     $('.category').change(function(){
-        // $(".loader").show();
+        articlesLoad = 12;
+        $(".articles").addClass("loader");
         $("header").addClass("header-small");
         $("footer").addClass("footer-small");
         $(".articles").empty();
@@ -12,31 +13,36 @@ $(function(){
             'api-key': "1f73ed52c30d4864bb4d5b310aec39fc"
             });
             url +=  "&fq=news_desk:('" + value + "')";
-        callAjax(url, 10);
+            // url +=  "&fq=news_desk:('Sports' 'Foreign')";
+            // url += "&fq=page:(0%201)";
+            
+        callAjax(url);
         // url += "&page=1";
-        // $(document).ajaxComplete(callAjax(url, 2));
-        // $(document).ajaxComplete($(".loader").hide());
+        // callAjax(url);
     });
 
-    function callAjax(url, noArticles) {  
+    function callAjax(url) {  
         $.ajax({
             method: "GET",
             url: url,
+            // async: false,
           }).done(function(data) {
+            //   console.log(data);
               var articles = data.response.docs;
               var image = "";
               var imageNumber = 0;
-              for (var i = 0; i < noArticles; i++) {
-                // testing subtype of multimedia to find the best quality picture
+              for (var i = 0; i < 10; i++) {
+                // testing subtype of multimedia to find the best quality picture (if there is any)
                 for (var j = 0; j < articles[i].multimedia.length; j++) {
                     if (articles[i].multimedia[j].subtype === "xlarge") {
                         image = articles[i].multimedia[j].subtype;
                         imageNumber = j;
+                        articlesLoad--;
                         break;
                     }
                 }
-                // appending the article to the list
-                if (image) {
+                // appending the article to the list if there is an image and no. of images left to load is >= 0
+                if (image&&(articlesLoad>=0)) {
                     $(".articles").append("<a href='" + articles[i].web_url + "' class='article' style='background:url(https://www.nytimes.com/" + articles[i].multimedia[imageNumber].url + ") no-repeat center/cover;'><li><p class='article-text'>" + articles[i].snippet + "</p></li></a>");
                 }  
               }
@@ -44,9 +50,4 @@ $(function(){
             throw error;
           });
     }
-
-    // Selectric
-    $(function() {
-        $('select').selectric();
-      });
 });
