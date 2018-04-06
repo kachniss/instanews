fb1268efca23e999f4295fb8b2aabd04998795f0
Kachniss <vopalkova.katerina@gmail.com>
2018-04-05 20:13:07 -0700

    Add babel, ES6

--- js/index.js
+++ js/index.js
@@ -2,52 +2,50 @@
 // MAIN JS FILE
 //
 
-$(function(){ 
-    var initialArticlesLoad = 12; // number of articles wanted
-    var articlesLoad = null;
+$(() => { 
+    const initialArticlesLoad = 12; // number of articles wanted
+    let articlesLoad = null;
     //
     // select field change
     // 
-    $('.category').change(function(){
-        getArticles();
-    });
+    $('.category').change(() => getArticles());
 
     //
     // loading articles: function for getting parameters before calling Ajax 
     //
-    function getArticles() {
+    const getArticles = () => {
         articlesLoad = initialArticlesLoad;
         $(".articles").addClass("loader");
         $("header").addClass("header-small");
         $("footer").addClass("footer-small");
         $(".search-bar").css("display", "flex");
         $(".articles").empty();
-        var section = $(".category").val();
-        var search = $(".search").val();
-        var apiKey = "1f73ed52c30d4864bb4d5b310aec39fc";
-        var url = "https://api.nytimes.com/svc/topstories/v2/" + section +".json?api-key=" + apiKey;
+        let section = $(".category").val();
+        let search = $(".search").val();
+        let apiKey = "1f73ed52c30d4864bb4d5b310aec39fc";
+        let url = `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${apiKey}`;
         callAjax(url, search);
     }
 
     //
     // Ajax call function
     //
-    function callAjax(url, search) {  
+    const callAjax = (url, search) => {  
         $.ajax({
             method: "GET",
             url: url,
-          }).done(function(data) {
-              var articles = data.results;
-              for (var i = 0; i < articles.length; i++) {
+          }).done((data) => {
+              let articles = data.results;
+              for (let article of articles) {
                 // finding the best quality picture (if there is any)
-                if(articles[i].multimedia.length === 5){
-                    var image = articles[i].multimedia[4].url;
-                    if (image&&(articlesLoad>0)) {
-                        var containsAbstract = isSubstring(articles[i].abstract, search);
-                        var containsTitle = isSubstring(articles[i].title, search);
+                if(article.multimedia.length === 5){
+                    let image = article.multimedia[4].url;
+                    if (image&&(articlesLoad > 0)) {
+                        let containsAbstract = isSubstring(article.abstract, search);
+                        let containsTitle = isSubstring(article.title, search);
                         // filter articles
                         if (!search || containsAbstract || containsTitle) {
-                            $(".articles").append("<li class='article'><a href='" + articles[i].url + "' style='background:url(" + image + ") no-repeat center/cover;'><p class='article-text'>" + articles[i].abstract + "</p></a></li>");
+                            $(".articles").append(`<li class='article'><a href='${article.url}' style='background:url(${image}) no-repeat center/cover;'><p class='article-text'>${article.abstract}</p></a></li>`);
                             articlesLoad--;
                         }  
                     }
@@ -61,7 +59,7 @@ $(function(){
                 $(".articles").html("<p class='no-article'>Sorry, no article found.</p>");
               }
               $(".articles").removeClass("loader");
-          }).fail(function(error) {
+          }).fail((error) => {
             throw error;
           });
     }
@@ -75,19 +73,17 @@ $(function(){
     // touchscreen detection
     // @source https://codeburst.io/the-only-way-to-detect-touch-with-javascript-7791a3346685/ 03/12/2018
     //
-    window.addEventListener('touchstart', function() {
-        $(".article-text").css("opacity", "1");
-    });
+    window.addEventListener('touchstart', () => $(".article-text").css("opacity", "1"));
 
     //
     // search input function
     // @source https://stackoverflow.com/questions/8747439/detecting-value-change-of-inputtype-text-in-jquery 03/15/2018
     //
-    var searchLength = $(".search").val().length;
+    let searchLength = $(".search").val().length;
 
-    $(".search").on("keyup", function(event) {
-        var key = event.keyCode;
-        var newSearchLenght = $(this).val().length;
+    $(".search").on("keyup", (event) => {
+        let key = event.keyCode;
+        let newSearchLenght = $(this).val().length;
         // alphanumeric input or backspace/delete which deletes a character
         if ((key <= 90 &&  key >= 48) || (key === 8 && searchLength !== newSearchLenght) || (key === 46 && searchLength !== newSearchLenght)) {
             getArticles();
@@ -95,11 +91,9 @@ $(function(){
         searchLength = newSearchLenght;
     });
 
-    $(".search").on("change paste", function() {
-        getArticles();
-    });
+    $(".search").on("change paste", () => getArticles());
 
-    $(".search").keypress(function(event) {
+    $(".search").keypress((event) => {
         if(event.keyCode === 13) {
             event.preventDefault();
         }
@@ -109,7 +103,5 @@ $(function(){
     // function for finding a substring (search input)
     // @source https://stackoverflow.com/questions/2854527/find-substring-with-jquery 03/15/2018
     //
-    function isSubstring(haystack, needle) {
-        return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
-    }
+    const isSubstring = (haystack, needle) => haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
 });
